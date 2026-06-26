b144bfdc24bbcbe57304297e9bd3c8a5cc686390
picklecai-pixel <picklecai@gmail.com>
2026-06-27 02:11:19 +0800

    Add portal route for Spark workspace

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import Portal from './Portal.jsx';
 import './styles.css';
+
+function Root() {
+  const [route, setRoute] = useState(() => window.location.hash || '#/');
+
+  useEffect(() => {
+    if (!window.location.hash) {
+      window.history.replaceState(null, '', '#/');
+    }
+
+    function handleHashChange() {
+      setRoute(window.location.hash || '#/');
+    }
+
+    window.addEventListener('hashchange', handleHashChange);
+    return () => window.removeEventListener('hashchange', handleHashChange);
+  }, []);
+
+  if (route.startsWith('#/account')) {
+    return (
+      <>
+        <a className="portal-back-link" href="#/">{'\u5de5\u4f5c\u53f0'}</a>
+        <App />
+      </>
+    );
+  }
+
+  return <Portal />;
+}
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 );
